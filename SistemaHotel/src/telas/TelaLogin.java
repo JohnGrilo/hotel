486f0a57487adb0c8c4b5f6e01f0016790e1b311
JohnGrilo <johngrilo@gmail.com>
2024-08-01 20:36:53 -0300

    Ajuste - Tela de Login

--- SistemaHotel/src/telas/TelaLogin.java
+++ SistemaHotel/src/telas/TelaLogin.java
@@ -107,6 +107,7 @@ public class TelaLogin extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
